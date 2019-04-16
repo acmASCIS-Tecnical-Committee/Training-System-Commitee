eef2a34c2014c50da96379f7c60373128b94e232
Mostafa <mostafa.negm.fcis@gmail.com>
2019-04-16 16:26:44 +0200

    Revert "clean mongoDb"
    This reverts commit d62eb03071998b13f240a930ebb74fc82cca260b.

--- config/mongoDb.js
+++ config/mongoDb.js
@@ -9,3 +9,43 @@ module.exports = {
 
   dataBaseClient: client
 };
+// module.exports = function hh(  req  , callk) {
+//     client.connect(err => {
+//         res = err;
+//         errors = err;
+//         if (!err) console.log("Connected successfully to MongoDB");
+//         else console.log("error : " + err);
+//         const db = client.db(dbName);
+
+//         callk(req);
+//         //insert
+
+//         // insertDocuments(db, cb => {
+//         //   //   console.log(cb);
+//         //   updateDoc(db, callback => {});
+//         // });
+
+//         //find all iteam in it
+
+//         // findAllDoc(db, cd => {
+//         //   //  console.log(cd);
+//         // });
+
+//         // find by cond
+
+//         // findDocBy(db, cd => {});
+
+//         // update doc By
+
+//         //updateDoc(db, callback => {});
+
+//         // perform actions on the collection object
+
+//         //remove one
+
+//         // removeDocument(db, cd => {});
+
+//         client.close();
+//     });
+
+// };
